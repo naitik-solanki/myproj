461beca8ffc3ccb3c97a19e3e2f8b2dfc8a36393
naitik-solanki <naitiksolanki2002@gmail.com>
2024-12-11 11:40:51 +0530

    added login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginPageComponent},
+  {path:'',component:CreateUserComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
